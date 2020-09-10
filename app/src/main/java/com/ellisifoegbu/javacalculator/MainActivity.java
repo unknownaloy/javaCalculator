c9e0ef9337d9b33ffa6033b7d83a32d14f562a3e
ELLIS <niceju4u@gmail.com>
2020-09-10 19:29:42 +0100

    Made some design changes to the calc UI in the XML file

--- app/src/main/java/com/ellisifoegbu/javacalculator/MainActivity.java
+++ app/src/main/java/com/ellisifoegbu/javacalculator/MainActivity.java
@@ -3,8 +3,23 @@ package com.ellisifoegbu.javacalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void calculate(View view) {
+        Button equalsButton = (Button) view;
+
+        Log.i("Equals button", equalsButton.getTag().toString());
+    }
+
+    public void computeDigits(View view) {
+        Button compute = (Button) view;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
